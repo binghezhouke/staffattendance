80d575f5a7dd5755e8a6ea08a36fa6d30db528be
binghezhouke <binghezhouke@gmail.com>
2023-03-05 12:00:39 +0800

    format

--- converter.py
+++ converter.py
@@ -14,8 +14,8 @@ LAUNCH_BONUS_END_TIME = datetime.strptime("12:30", "%H:%M")
 def judge(cell):
     font_late = Font(color="FF0000")
     font_too_early = Font(color="FF0000")
-    font_bonus = Font(color="912CEE")
-    font_holiday=Font(color="00FF00")
+    # font_bonus = Font(color="912CEE")
+    font_holiday = Font(color="00FF00")
 
     too_early_delta = timedelta(hours=9)
     bonus_delta = timedelta(hours=11)
@@ -39,18 +39,17 @@ def judge(cell):
             cell.font = font_too_early
         if end-start >= bonus_delta:
             should_give_bonus = True
-            #cell.font = font_bonus
+            # cell.font = font_bonus
 
         if start <= LAUNCH_BONUS_END_TIME and end >= DAY_END_TIME:
             should_give_launch_bonus = True
         cell.value = "{}\n{}".format(s, e)
     else:
         if "休息" in s:
-            cell.font=font_holiday
+            cell.font = font_holiday
         else:
             cell.font = font_late
             cell.value = "{}\n".format(v[0].strip())
-
 
     return should_give_bonus, should_give_launch_bonus
 
@@ -63,11 +62,12 @@ def find_holiday(ws, row_max, col_max):
         if holiday_cnt >= 1:
             holidays_idx.append(c)
     print("本月休息日为:")
-    month_days=col_max-COL_START
-    wokr_days=month_days-len(holidays_idx)+1
+    month_days = col_max-COL_START
+    wokr_days = month_days-len(holidays_idx)+1
     print([x-COL_START+1 for x in holidays_idx])
     print(wokr_days)
     return holidays_idx
+
 
 def handle(in_file, out_file):
     wb = load_workbook(in_file)
@@ -75,8 +75,8 @@ def handle(in_file, out_file):
     row_max, col_max = ws.max_row, ws.max_column
 
     holiday_idx = find_holiday(ws, row_max, col_max)
-    ws.cell(ROW_START-1, col_max+1).value="晚餐补助"
-    ws.cell(ROW_START-1, col_max+2).value="午餐补助"
+    ws.cell(ROW_START-1, col_max+1).value = "晚餐补助"
+    ws.cell(ROW_START-1, col_max+2).value = "午餐补助"
     for r in range(ROW_START, row_max+1):
         print(ws.cell(r, 1).value)
         bonus_cnt = 0
